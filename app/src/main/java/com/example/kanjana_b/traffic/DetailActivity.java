88d1f19013535432aa71cbcb4239b2b739706d1b
kanjana <kanjana_bs@hotmail.com>
2015-12-22 16:34:44 +0700

    Finish

---
+++ app/src/main/java/com/example/kanjana_b/traffic/DetailActivity.java
@@ -0,0 +1,50 @@
+package com.example.kanjana_b.traffic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    //Explicit
+    private TextView titleTextView, detailTextView;
+    private ImageView trafficImageView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        //Bind Widget
+        bindWidget();
+
+        //Show View
+        showView();
+    }//Main Method
+
+    private void showView() {
+
+        //Show Title
+        String strTitle = getIntent().getStringExtra("Title");
+        titleTextView.setText(strTitle);
+
+        //Show Image
+        int intIcon = getIntent().getIntExtra("Image", R.drawable.traffic_01);
+        trafficImageView.setImageResource(intIcon);
+
+        //Show Detail
+        String[] strDetail = getResources().getStringArray(R.array.detail_long);
+        int intIndex = getIntent().getIntExtra("Index", 0);
+        detailTextView.setText(strDetail[intIndex]);
+
+    }
+
+    private void bindWidget() {
+        titleTextView = (TextView) findViewById(R.id.textView4);
+        detailTextView = (TextView) findViewById(R.id.textView5);
+        trafficImageView = (ImageView) findViewById(R.id.imageView2);
+    }
+
+}//Main Class
